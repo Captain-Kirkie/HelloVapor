13b5064d9e77889f33dba40dd723b453f5fb50d2
kirkH <kirk@passivelogic.com>
2023-01-07 16:27:11 -0700

    adding new routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,14 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", "vapor") { req async -> String in
+        "Hello Vapor"
+    }
+    // dynamic...
+    app.get("hello", ":name") { req async -> String in
+        let name = try! req.parameters.require("name")
+        return "Hello \(name)"
+    }
+    
 }
