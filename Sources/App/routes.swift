6108d2b2b5a9775a0a543fc5be491461c93cfe31
kirkH <kirk@passivelogic.com>
2023-01-07 17:02:29 -0700

    added get date route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -19,6 +19,11 @@ func routes(_ app: Application) throws {
         return "Hello \(name)"
     }
     
+    app.get("date") { req async -> String in
+        let currentDate = formatDate()
+        return currentDate
+    }
+    
     // MARK: POST
     app.post("info") { req -> InfoResponse in
         let data = try req.content.decode(InfoData.self)
@@ -35,4 +40,14 @@ struct InfoResponse: Content {
     let request: InfoData
 }
 
-
+func formatDate() -> String {
+    let months : [String] = ["January","Febrary","March","April","May","June","July","August","September","October","November","December"]
+    let date = Date()
+    let calendar = Calendar.current
+    let hour = calendar.component(.hour, from: date)
+    let minutes = calendar.component(.minute, from: date)
+    let month = calendar.component(.month, from: date)
+    let day = calendar.component(.day, from: date)
+    let year = calendar.component(.year, from: date)
+    return "today is \(months[month - 1]) \(day) \(year) \(hour):\(minutes)"
+}
